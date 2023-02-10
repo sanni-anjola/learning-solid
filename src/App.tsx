b39d684ce19cb1de306f6e1a1369436d32d37169
unknown <sanni.anjola@gmail.com>
2023-02-11 00:09:56 +0100

    add increment counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import type { Component } from 'solid-js';
+import { Component, createSignal } from 'solid-js';
 
 
 const App: Component = () => {
+  const [getCounter, setCounter] = createSignal(0)
   return (
     <div>
-      <h1>Hello Solid</h1>
+      <h1>Hello Solid - {getCounter()}</h1>
+      <button onClick={() => setCounter(prev => prev + 1)}>Increment</button>
     </div>
   );
 };
